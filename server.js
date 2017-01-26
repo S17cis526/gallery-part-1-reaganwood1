f858acd68b1d5d6d1dd6518c1a2569bca8089ae8
Reagan Wood <reaganwood1@WIN2.CS.KSU.EDU>
2017-01-26 13:26:16 -0600

    added server code to server.js

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+"use strict";
+
+// 'hello ${name}! will print out the variable name concatonated to the string
 /**
  * server.js
  * This file defines the server for a
  * simple photo gallery web app.
  */
  
+ var http = require ('http'); // when require is seen, the type will be http.
+ var port = 3000;
+ 
+ var server = http.createServer((req, res) =>{
+	res.end("OK");
+ });
+
+ server.listen(port, () => {
+	 
+	 console.log("listening on Port " + port);
+ });
