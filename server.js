85b9b4a1f43c8b7206dd9d0c687c54082adaa823
Reagan Wood <reaganwood1@WIN2.CS.KSU.EDU>
2017-01-31 14:27:16 -0600

    Added code to add a gallery of images to the webpage

--- server.js
+++ server.js
@@ -11,7 +11,8 @@
  var fs = require ('fs');
  
  var port = 2998;
- 
+ var stylesheet = fs.readFileSync('galler.css');
+ var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
  function serveImage(filename, req, res) {
 	 fs.readFile('images/' + filename, function(err, body){
 		 
@@ -24,19 +25,40 @@
 		 }
 		 
 	 // stores binary date of image data
-			res.setheader("Contet-Type", "image/jpeg"); // tells browser what the data is
+			res.setHeader("Contet-Type", "image/jpeg"); // tells browser what the data is
 			res.end(body); // browser will now display data
 	 }); 
 	 
  }
  var server = http.createServer(function(req, res) {
 	switch (req.url) {
-		
+		case "/gallery":
+	var gHtml = imageNames.map(function(fileName){
+		return ' <img src="' + fileName + '" alt="a fishing ace at work">'
+		}).join('');// takes a function thats going to be called, and populates a new array
+			var html = '<!doctype html>'
+				html += '<head><title>Dynamic Page</title><link href="galler.css" rel = "stylesheet" type="text/css"></head>';
+				html += '<h1>Hello.</h2> Time is ' + Date.now();
+				html += '<body>';
+				html += ' <h1>Gallery</h1>';
+				html += gHtml;
+				html += '</body>';
+				res.setHeader('Content-Type', 'text/html');
+			res.end(html);
+			break;
 		case "/chess":
+			
 			serveImage('chess.jpg', req, res);
 			break;
-		case "/fern.jpeg":
+		case "/fern":
 			serveImage('fern.jpg', req, res);
+			break;
+		case "/ace":
+			serveImage('ace.jpg', req, res);
+			break;
+		case '/galler.css':
+			res.setHeader('Content-Type', 'text/css');
+			res.end(stylesheet);
 			break;
 		default:
 			res.statusCode = 404;
