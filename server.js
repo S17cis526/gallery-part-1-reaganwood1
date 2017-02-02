b142a5f9978b1cf140b515e0d5ec532d4a9f0f53
Reagan Wood <reaganwood1@WIN2.CS.KSU.EDU>
2017-02-02 14:15:57 -0600

    Added code to dynamically create images through callback functions

--- server.js
+++ server.js
@@ -13,12 +13,58 @@
  var port = 2998;
  var stylesheet = fs.readFileSync('galler.css');
  var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
+ function getImageNames(callback) {
+	 
+	 fs.readdir('images/', function(err, fileNames){
+		 if (err) {
+			 callback(err, undefined);
+		 } else {
+			 
+			 callback(undefined, fileNames);
+		 }
+	 });
+ }
+ function buildGallery(imageTags) {
+			
+			var html = '<!doctype html>';
+				html += '<head><title>Dynamic Page</title><link href="galler.css" rel = "stylesheet" type="text/css"></head>';
+				html += '<h1>Hello.</h2> Time is ' + Date.now();
+				html += '<body>';
+				html += ' <h1>Gallery</h1>';
+				html += imageNamesToTags(imageTags).join('');
+				html += '</body>';
+			return html;
+			
+ }
+ 
+ function serveGallery(req, res) {
+	 getImageNames(function(err, imageNames) {
+		 if (err) {
+			 console.err(err);
+			 res.statusCode = 500;
+			 res.statusMessage = 'Server error';
+			 res.end();
+			 return;
+		 }
+		 res.setHeader('Content-Type', 'text/html');
+		 res.end(buildGallery(imageNames));
+	 });
+	 
+ }
+ function imageNamesToTags(fileNames) {
+	 
+	 return fileNames.map(function(fileName) {
+		 
+		 return '<img src="' + fileName + '"" alt="' + fileName + '">';
+	 });
+	 
+ }
  function serveImage(filename, req, res) {
 	 fs.readFile('images/' + filename, function(err, body){
 		 
 		 if (err){
 			 console.error(err);
-			 res.statusCode = 500;
+			 res.statusCode = 404;
 			 res.statusMessage = "whoops";
 			 res.end("Silly me");
 			 return;
@@ -33,37 +79,15 @@
  var server = http.createServer(function(req, res) {
 	switch (req.url) {
 		case "/gallery":
-	var gHtml = imageNames.map(function(fileName){
-		return ' <img src="' + fileName + '" alt="a fishing ace at work">'
-		}).join('');// takes a function thats going to be called, and populates a new array
-			var html = '<!doctype html>'
-				html += '<head><title>Dynamic Page</title><link href="galler.css" rel = "stylesheet" type="text/css"></head>';
-				html += '<h1>Hello.</h2> Time is ' + Date.now();
-				html += '<body>';
-				html += ' <h1>Gallery</h1>';
-				html += gHtml;
-				html += '</body>';
-				res.setHeader('Content-Type', 'text/html');
-			res.end(html);
+			serveGallery(req, res);
 			break;
-		case "/chess":
-			
-			serveImage('chess.jpg', req, res);
-			break;
-		case "/fern":
-			serveImage('fern.jpg', req, res);
-			break;
-		case "/ace":
-			serveImage('ace.jpg', req, res);
-			break;
+		case '/':
 		case '/galler.css':
 			res.setHeader('Content-Type', 'text/css');
 			res.end(stylesheet);
 			break;
 		default:
-			res.statusCode = 404;
-			res.statusMessage = "Not found";
-			res.end(); // can only call this once
+			serveImage(req.url, req, res);
 	}
 	
  });
